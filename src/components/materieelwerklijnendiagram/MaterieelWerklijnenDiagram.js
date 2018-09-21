292426193d93d24c10f9b7810f82bb1c4a6a180e
Koen de Vries <koen.de.vries@outlook.com>
2018-09-21 14:19:08 +0200

    Tabel toegevoegd aan Defectenoverzicht

--- src/components/materieelwerklijnendiagram/MaterieelWerklijnenDiagram.js
+++ src/components/materieelwerklijnendiagram/MaterieelWerklijnenDiagram.js
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export const MaterieelWerklijnenDiagramComponent = (props) => {
   return (
-    <h2>Hello Mr. {props.userName} !</h2>
+    <h2>Hello Mr. {props.userName}!</h2>
   );
 }
